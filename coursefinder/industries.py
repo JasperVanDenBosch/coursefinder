4cdb2b542fbf2e4367bd86827ba67c81556a5862
Jasper <japsai@gmail.com>
2018-04-25 17:43:28 +0200

    Industries Dataframe now includes division

--- coursefinder/industries.py
+++ coursefinder/industries.py
@@ -15,6 +15,31 @@ industries.dropna(subset=levels, thresh=1, inplace=True)
 # create a column with the hierarchy level 
 level = industries[['lvl1', 'lvl2', 'lvl3']].stack().index.get_level_values(1)
 industries['level'] = level
+industries.set_index('SIC', inplace=True)
 
+# Determine code range by division
+divisions = {}
+division = None
+startCode = None
+code = 100
+with open('data/industries.csv') as fhandle:
+    for line in fhandle.readlines()[1:]:
+        cell = line.split(',')[0]
+        if cell.isnumeric():
+            code = int(cell)
+        elif len(cell) > 5:
+            if division is not None:
+                divisions[division] = (startCode, code)
+            division = cell.strip('"')
+            startCode = code + 1
+    # last one
+    divisions[division] = (startCode, code)
+
+
+labels = [' '.join(k.split()[2:]) for k in sorted(divisions.keys())]
+bins = [div[1][0] for div in sorted(divisions.items())]
+bins += [10000]
+divByIndustry = pandas.cut(industries.index, bins=sorted(bins), labels=labels)
+industries['div'] = divByIndustry
 
 
