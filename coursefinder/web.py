c0390c68a96ea3853bfcb33a2b1b68297cc0dfab
Jasper <japsai@gmail.com>
2018-04-30 01:11:49 +0200

    Adding views in main, instead of using scan()

--- coursefinder/web.py
+++ coursefinder/web.py
@@ -9,19 +9,11 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600*48)
-
     config.add_route('home', '/')
-    # config.add_route('my/experiment/task/asset', '/asset/{aid}')
-    # webtasks.filters.addFilters(config)
-    # webtasks.helpers.addHelpers(config)
-
-    config.scan()
+    config.add_view(home, route_name='home', renderer='templates/home.jinja2')
     return config.make_wsgi_app()
 
-from pyramid.view import view_config
 
-
-@view_config(route_name='home', renderer='templates/home.jinja2')
 def home(request):
     return {}
 
